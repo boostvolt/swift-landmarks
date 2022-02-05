153afdc36dc7f2e567acd3fe034b742256c368fb
Boostvolt <51777660+boostvolt@users.noreply.github.com>
2022-02-06 00:05:14 +0100

    Creating and Combining Views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jan Kott on 05.02.22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
